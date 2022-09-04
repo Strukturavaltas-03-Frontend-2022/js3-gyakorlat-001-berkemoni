847b853287a4343fb38a01d4030a08ab1e98b523
Moni <monika.berke@gmail.com>
2022-09-04 21:11:28 +0200

    modified

--- 08-other/exercises/01/solution/js/removeDuplicatesFromArray.js
+++ 08-other/exercises/01/solution/js/removeDuplicatesFromArray.js
@@ -1,1 +1,14 @@
+'use strict';
+
+/*
+1. Írj egy függvény kifejezést `removeDuplicatesFromArray` néven, 
+ami a paraméterként megadott tömbből eltávolítja a duplikált elemeket! 
+A tömb csak primítív értékeket tartalmaz! 
+A függvény visszatérési értéke a duplikált elemektől mentes új tömb!
+*/
+
+function removeDuplicatesFromArray() {
+    [...new Set(arr)];
+}
+
 export default removeDuplicatesFromArray;
--- 08-other/exercises/03/solution/js/objectConverter.js
+++ 08-other/exercises/03/solution/js/objectConverter.js
@@ -1,1 +1,36 @@
+'use strict';
+
+
+const objectConverter = {
+    arrayToMap(array) {
+        const arrMap = new Map([]);
+        array.forEach((val, i) => arrMap.set(i, val));
+        return arrMap;
+    },
+    arrayToSet(array) {
+        return new Set(array);
+    },
+    setToMap(set) {
+        const setMap = new Map([]);
+        Array.from(set).forEach((val, i) => setMap.set());
+        return setMap;
+    },
+    setToArray(set) {
+        return [...set]
+    },
+    mapToArray(map) {
+        const mapArray = [];
+        map.forEach((value) => {
+            mapArray.push(value);
+        });
+        return mapArray;
+    },
+    mapToSet(map) {
+        const mapSet = [];
+        map.forEach((value) => mapSet.add(value));
+        return new Set(mapSet);
+    },
+}
+
+
 export default objectConverter;
